9f4046d0c1b4508d894b1a978a7a12137fbad5f6
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-21 17:39:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def consolidate_cart(cart)
     item_name = cart [i][:item]
     sought_item = find_item_by_name_in_collection(item_name, result)
   if sought_item
-    sought_item[:count] = 1 
+    sought_item[:count] += 1 
   else 
     cart[i][:count] = 1 
     result << cart[i]
